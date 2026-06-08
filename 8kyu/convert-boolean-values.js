bdab436989c2991f3e094459e3f350a85f598ca0
denMarch1988 <denis.marchienko.1988@gmail.com>
2026-06-08 21:18:11 +0300

    3

---
+++ 8kyu/convert-boolean-values.js
@@ -0,0 +1,10 @@
+// Описание:
+// Завершите метод, который принимает логическое значение и возвращает "Yes"строку для true, или "No"строку для false.
+
+// Основы
+
+function boolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+}
+
+console.log(boolToWord(false));
